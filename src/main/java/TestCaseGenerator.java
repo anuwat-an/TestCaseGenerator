0154bf151086c5f33673fd4d66451b2765792f91
moiammo <yarnadhis.k@ku.th>
2018-02-05 22:57:54 +0700

    to do : output re-arranging

--- src/main/java/TestCaseGenerator.java
+++ src/main/java/TestCaseGenerator.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,31 +14,79 @@ public class TestCaseGenerator {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.print("Enter minimum of X: ");
-        int minX = Integer.parseInt(buffer.readLine());
+        double minX = Integer.parseInt(buffer.readLine());
         System.out.print("Enter maximum of X: ");
-        int maxX = Integer.parseInt(buffer.readLine());
+        double maxX = Integer.parseInt(buffer.readLine());
         System.out.print("Enter minimum of Y: ");
-        int minY = Integer.parseInt(buffer.readLine());
+        double minY = Integer.parseInt(buffer.readLine());
         System.out.print("Enter maximum of Y: ");
-        int maxY = Integer.parseInt(buffer.readLine());
-
-        int[] xValues = new int[]{minX, minX+1, Math.round((minX+maxX)/2), maxX-1, maxX};
-        int[] yValues = new int[]{minY, minY+1, Math.round((minY+maxY)/2), maxY-1, maxY};
-
-        int midX = xValues[xValues.length/2];
-        int midY = yValues[yValues.length/2];
-
+        double maxY = Integer.parseInt(buffer.readLine());
+        System.out.println("1: bva");
+        System.out.println("2: robustness");
+        System.out.println("3: worst case");
+        System.out.println("4: worst case robustness");
+        System.out.print("Select mode : ");
+        int mode = Integer.parseInt(buffer.readLine());
+        double midX ;
+        double midY ;
+        ArrayList<Double> xValues = new ArrayList<Double>();
+        ArrayList<Double> yValues = new ArrayList<Double>();
+        ArrayList<String> outputTemp = new ArrayList<String>();
         Map<String, String> testCases = new HashMap<String, String>();
 
-        for (int x : xValues) {
-            testCases.put(x + " " + midY, (x+midY)+"");
+    if (mode == 1) { //bva
+        xValues.add(minX); xValues.add(minX+1); xValues.add((minX+maxX)/2); xValues.add(maxX-1); xValues.add(maxX);
+        yValues.add(minY); yValues.add(minY+1); yValues.add((minY+maxY)/2); yValues.add(maxY-1); yValues.add(maxY);
+        int midIndex = Math.round(xValues.size()/2) ;
+        midX = xValues.get(midIndex);
+        midY = xValues.get(midIndex);
+        for (double x : xValues) { testCases.put(x + " " + midY, (x + midY) + ""); }
+        for (double y : yValues) { testCases.put(midX + " " + y, (midX + y) + ""); }
+    }
+    else if (mode == 2) { //robust
+        xValues.add(minX-1); xValues.add(minX); xValues.add(minX+1); xValues.add((minX+maxX)/2); xValues.add(maxX-1); xValues.add(maxX); xValues.add(maxX+1);
+        yValues.add(minY-1); yValues.add(minY); yValues.add(minY+1); yValues.add((minY+maxY)/2); yValues.add(maxY-1); yValues.add(maxY); yValues.add(maxY+1);
+        int midIndex = Math.round(xValues.size()/2) ;
+        midX = xValues.get(midIndex);
+        midY = xValues.get(midIndex);
+        for (double x : xValues) { testCases.put(x + " " + midY, (x + midY) + ""); }
+        for (double y : yValues) { testCases.put(midX + " " + y, (midX + y) + ""); }
+    }
+
+    else if (mode == 3 ) { // worst case
+        xValues.add(minX); xValues.add(minX+1); xValues.add((minX+maxX)/2); xValues.add(maxX-1); xValues.add(maxX);
+        yValues.add(minY); yValues.add(minY+1); yValues.add((minY+maxY)/2); yValues.add(maxY-1); yValues.add(maxY);
+        for (double x : xValues){
+            for (double y : yValues){
+                testCases.put(x + " " + y, x+y+"");
+            }
         }
-        for (int y : yValues) {
-            testCases.put(midX + " " + y, (midX+y)+"");
+    }
+
+    else if (mode == 4) { // worst case robustness
+        xValues.add(minX-1); xValues.add(minX); xValues.add(minX+1); xValues.add((minX+maxX)/2); xValues.add(maxX-1); xValues.add(maxX); xValues.add(maxX+1);
+        yValues.add(minY-1); yValues.add(minY); yValues.add(minY+1); yValues.add((minY+maxY)/2); yValues.add(maxY-1); yValues.add(maxY); yValues.add(maxY+1);
+        for (double x : xValues){
+            for (double y : yValues){
+                testCases.put(x + " " + y, x+y+"");
+            }
         }
+    }
+
 
         PrintWriter inputsWriter = new PrintWriter("inputs.txt", "UTF-8");
         PrintWriter outputsWriter = new PrintWriter("outputs.txt", "UTF-8");
+        for (String key : testCases.keySet()){
+            if (!(outputTemp.contains(testCases.get(key)))){
+                outputTemp.add(testCases.get(key));
+            }
+        }
+//        outputTemp = outputTemp.sort(list, new Comparator<Message>() {  help me sort dis m8
+//            @Override
+//            public int compare(Message o1, Message o2) {
+//                return o1.getTime() - o2.getTime();
+//            }
+//        });
         for (String input : testCases.keySet()) {
             inputsWriter.println(input);
             outputsWriter.println(testCases.get(input));
@@ -45,6 +94,7 @@ public class TestCaseGenerator {
 
         inputsWriter.close();
         outputsWriter.close();
+        System.out.println("Done.Please take a look at 'input.txt' and 'output.txt'.");
 
     }
 }
